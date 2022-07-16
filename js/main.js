05336fe0b5c2533a2f87ab6541892e13ba09aaa4
EslamEs1 <mekky.web22@gmail.com>
2022-07-16 02:56:12 +0200

    index.html

--- js/main.js
+++ js/main.js
@@ -1,276 +1,42 @@
-$(document).ready(function () {
-    $(".items").slick({
-        dots: true,
-        infinite: true,
-        speed: 800,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        slidesToShow: 6,
-        slidesToScroll: 2,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
-    });
-});
+`use strict`;
+let dataTime = new Date();
+let hour = dataTime.getHours();
+let minute = dataTime.getMinutes();
+let day = dataTime.getDate();
+let month = dataTime.getMonth();
+let year = dataTime.getFullYear();
+let week = dataTime.getDay();
+let weekDay = ["السبت", "الأحد", "الأثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة"];
+let monthDay = ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
+let hourText = ["صباحاً", "مساءً"];
 
-// Start data Time
-$(document).ready(function () {
-    $("ul.tabs li").click(function () {
-        var tab_id = $(this).attr("data-tab");
-
-        $("ul.tabs li").removeClass("current");
-        $(".tab-content").removeClass("current");
-
-        $(this).addClass("current");
-        $("#" + tab_id).addClass("current");
-    });
-});
-
-// Start Cleander
-(function ($) {
-    "use strict";
-
-    document.addEventListener(
-        "DOMContentLoaded",
-        function () {
-            var today = new Date(),
-                year = today.getFullYear(),
-                month = today.getMonth(),
-                monthTag = [
-                    "يناير",
-                    "فبراير",
-                    "مارس",
-                    "إبريل",
-                    "مايو",
-                    "يونيو",
-                    "يوليو",
-                    "أغسطس",
-                    "سبتمبر",
-                    "اكتوبر",
-                    "نوفمبر",
-                    "ديسمبر",
-                ],
-                day = today.getDate(),
-                days = document.getElementsByTagName("td"),
-                selectedDay,
-                setDate,
-                daysLen = days.length;
-            // options should like '2014-01-01'
-            function Calendar(selector, options) {
-                this.options = options;
-                this.draw();
-            }
-
-            Calendar.prototype.draw = function () {
-                this.getCookie("selected_day");
-                this.getOptions();
-                this.drawDays();
-                var that = this,
-                    reset = document.getElementById("reset"),
-                    pre = document.getElementsByClassName("pre-button"),
-                    next = document.getElementsByClassName("next-button");
-
-                pre[0].addEventListener("click", function () {
-                    that.preMonth();
-                });
-                next[0].addEventListener("click", function () {
-                    that.nextMonth();
-                });
-                reset.addEventListener("click", function () {
-                    that.reset();
-                });
-                while (daysLen--) {
-                    days[daysLen].addEventListener("click", function () {
-                        that.clickDay(this);
-                    });
-                }
-            };
-
-            Calendar.prototype.drawHeader = function (e) {
-                var headDay = document.getElementsByClassName("head-day"),
-                    headMonth = document.getElementsByClassName("head-month");
-
-                e ? (headDay[0].innerHTML = e) : (headDay[0].innerHTML = day);
-                headMonth[0].innerHTML = monthTag[month] + " - " + year;
-            };
-
-            Calendar.prototype.drawDays = function () {
-                var startDay = new Date(year, month, 1).getDay(),
-                    //      下面表示这个月总共有几天
-                    nDays = new Date(year, month + 1, 0).getDate(),
-                    n = startDay;
-                //      清除原来的样式和日期
-                for (var k = 0; k < 42; k++) {
-                    days[k].innerHTML = "";
-                    days[k].id = "";
-                    days[k].className = "";
-                }
-
-                for (var i = 1; i <= nDays; i++) {
-                    days[n].innerHTML = i;
-                    n++;
-                }
-
-                for (var j = 0; j < 42; j++) {
-                    if (days[j].innerHTML === "") {
-                        days[j].id = "disabled";
-                    } else if (j === day + startDay - 1) {
-                        if (
-                            (this.options &&
-                                month === setDate.getMonth() &&
-                                year === setDate.getFullYear()) ||
-                            (!this.options &&
-                                month === today.getMonth() &&
-                                year === today.getFullYear())
-                        ) {
-                            this.drawHeader(day);
-                            days[j].id = "today";
-                        }
-                    }
-                    if (selectedDay) {
-                        if (
-                            j === selectedDay.getDate() + startDay - 1 &&
-                            month === selectedDay.getMonth() &&
-                            year === selectedDay.getFullYear()
-                        ) {
-                            days[j].className = "selected";
-                            this.drawHeader(selectedDay.getDate());
-                        }
-                    }
-                }
-            };
-
-            Calendar.prototype.clickDay = function (o) {
-                var selected = document.getElementsByClassName("selected"),
-                    len = selected.length;
-                if (len !== 0) {
-                    selected[0].className = "";
-                }
-                o.className = "selected";
-                selectedDay = new Date(year, month, o.innerHTML);
-                this.drawHeader(o.innerHTML);
-                this.setCookie("selected_day", 1);
-            };
-
-            Calendar.prototype.preMonth = function () {
-                if (month < 1) {
-                    month = 11;
-                    year = year - 1;
-                } else {
-                    month = month - 1;
-                }
-                this.drawHeader(1);
-                this.drawDays();
-            };
-
-            Calendar.prototype.nextMonth = function () {
-                if (month >= 11) {
-                    month = 0;
-                    year = year + 1;
-                } else {
-                    month = month + 1;
-                }
-                this.drawHeader(1);
-                this.drawDays();
-            };
-
-            Calendar.prototype.getOptions = function () {
-                if (this.options) {
-                    var sets = this.options.split("-");
-                    setDate = new Date(sets[0], sets[1] - 1, sets[2]);
-                    day = setDate.getDate();
-                    year = setDate.getFullYear();
-                    month = setDate.getMonth();
-                }
-            };
-
-            Calendar.prototype.reset = function () {
-                month = today.getMonth();
-                year = today.getFullYear();
-                day = today.getDate();
-                this.options = undefined;
-                this.drawDays();
-            };
-
-            Calendar.prototype.setCookie = function (name, expiredays) {
-                if (expiredays) {
-                    var date = new Date();
-                    date.setTime(
-                        date.getTime() + expiredays * 24 * 60 * 60 * 1000
-                    );
-                    var expires = "; expires=" + date.toGMTString();
-                } else {
-                    var expires = "";
-                }
-                document.cookie =
-                    name + "=" + selectedDay + expires + "; path=/";
-            };
-
-            Calendar.prototype.getCookie = function (name) {
-                if (document.cookie.length) {
-                    var arrCookie = document.cookie.split(";"),
-                        nameEQ = name + "=";
-                    for (var i = 0, cLen = arrCookie.length; i < cLen; i++) {
-                        var c = arrCookie[i];
-                        while (c.charAt(0) == " ") {
-                            c = c.substring(1, c.length);
-                        }
-                        if (c.indexOf(nameEQ) === 0) {
-                            selectedDay = new Date(
-                                c.substring(nameEQ.length, c.length)
-                            );
-                        }
-                    }
-                }
-            };
-            var calendar = new Calendar();
-        },
-        false
-    );
-})(jQuery);
+let x = document.getElementById("time");
+x.innerHTML = `${hourText[hour > 12 ? 1 : 0]}، ${hour > 12 ? hour - 12 : hour}:${minute}، ${weekDay[week]}، ${day} ${monthDay[month]} ${year}`;
 
 
 
 
-(function ($) {
-    "use strict";
 
-    var fullHeight = function () {
-        $(".js-fullheight").css("height", $(window).height());
-        $(window).resize(function () {
+
+
+
+
+
+    (function ($) {
+        "use strict";
+
+        var fullHeight = function () {
             $(".js-fullheight").css("height", $(window).height());
+            $(window).resize(function () {
+                $(".js-fullheight").css("height", $(window).height());
+            });
+        };
+        fullHeight();
+
+        $("#sidebarCollapse").on("click", function () {
+            $("#sidebar").toggleClass("active");
         });
-    };
-    fullHeight();
-
-    $("#sidebarCollapse").on("click", function () {
-        $("#sidebar").toggleClass("active");
-    });
-})(jQuery);
-
-
+    })(jQuery);
 
 // Start Slider
 const slider = document.querySelector(".slider");
@@ -298,9 +64,4 @@ slider.addEventListener("mousemove", (e) => {
     const x = e.pageX;
     const dragged = x - startX;
     slider.scrollLeft = sLeft - dragged;
-}); 
-
-
-
-
-
+});
